4ef6ac0b0c6b6450a6c0a7e22fda9e8b773690ee
GST2009 <mailstogst11@gmail.com>
2025-04-03 12:48:57 +0530

    Add files via upload

---
+++ Assignment-1/Assignment 1.py
@@ -0,0 +1,14 @@
+#TASK 1
+A=int(input("Enter a number: "))
+B=int(input("Enter second number: "))
+C=A+B
+print("addition = ",A+B)
+print("subtraction = ",A-B)
+print("multiplication = ",A*B)
+print("Division= ",A/B)
+
+#TASK 2
+A=input("Enter your first name: ")
+B=input("Enter your second name: ")
+C=A+B
+print("Hello,",C,"!Welcome to Python program.")
